8e839e945c62e3df80ada4bd07d259f4fa9d61a0
Tim Gabriel <tim.gabriel@t-online.de>
2017-04-24 16:57:56 +0200

    added preSpawner and preCheckpoint

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    [Range(1, 1000)]
+    [Header("Geschwindigkeit")]
+    public float playerSpeed = 100;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Range(-100, 0)]
+    [Header("Minimalhöhe")]
+    public float bottomLevel = -5;
+
+    Rigidbody rigid_body;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid_body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        rigid_body.AddForce(movement * playerSpeed * Time.deltaTime);
+
+        if(transform.position.y < bottomLevel)
+        {
+            Debug.Log("Leider verloren");
+            Debug.Log("Neuer Versuch!");
+            GameObject.FindObjectOfType<Spawner>().spawn();
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+    [Header("Start-Spawn")]
+    public GameObject firstSpawner;
+
+	void Start () {
+        //firstSpawner
+    }
+	
+	void Update () {
+		
+	}
+}
