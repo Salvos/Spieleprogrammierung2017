9de3b47376c41eb725dacdc9af1db6f0f15e4962
Salvos <tim.gabriel@t-online.de>
2017-04-29 18:18:09 +0200

    code commenting

--- Assets/MyAssets/Scripts/Spawner.cs
+++ Assets/MyAssets/Scripts/Spawner.cs
@@ -7,25 +7,38 @@ public class Spawner : MonoBehaviour {
     public GameObject playerobject;
     private Animator animator;
     public GameObject prePlayer;
+
+    // Sets the Color for initialization
     private Color preColor = new Color(1, 1, 1);
 
-	// Use this for initialization
-	void Start () {
-        //spawn();
+    /// <summary>
+    /// Use this for initialization
+    /// get the animator component for the animation
+    /// </summary>
+    void Start () {
         animator = GetComponent<Animator>();
-
     }
 	
+    /// <summary>
+    /// spawn component
+    /// gets triggered after the animation
+    /// "spawn" the player object up the object
+    /// </summary>
     public void spawn()
     {
-        GameObject player = GameObject.Instantiate(playerobject, transform.position + Vector3.down, Quaternion.identity);
+        GameObject player = GameObject.Instantiate(playerobject, transform.position + Vector3.up, Quaternion.identity);
         player.GetComponent<MeshRenderer>().material.color = preColor;
     }
 
+    /// <summary>
+    /// respawns the player
+    /// </summary>
     public void respawn()
     {
+        // play the animation
         animator.SetTrigger("respawn");
 
+        // change the color (not important - can be removed later)
         MeshRenderer mr = prePlayer.GetComponent<MeshRenderer>();
         float r = Random.Range(0f, 1f);
         float g = Random.Range(0f, 1f);
