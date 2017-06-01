0fa924c71792d731acc077a3d6f76be7fdc08e59
Salvos <tim.gabriel@t-online.de>
2017-06-01 18:46:47 +0200

    just forgot the collider & the script - fix

--- Assets/MyAssets/Scripts/ColliderDeath.cs
+++ Assets/MyAssets/Scripts/ColliderDeath.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class ColliderDeath : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    ///==================================///
+    ///==========Vars & Objects==========///
+    ///==================================///
+
+    /// <summary>
+    /// variable for gamecontroller to notificate the script if the player enters the finish block
+    /// </summary>
+    private GameController gameController;
+
+
+
+    ///==================================///
+    ///==========Unity Methods==========///
+    ///==================================///
+
+    /// <summary>
+    /// initialization for the gamecontroller
+    /// get it from the main camera object
+    /// </summary>
+    void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    /// <summary>
+    /// triggers on collision with the player
+    /// </summary>
+    /// <param name="col"></param>
+    void OnCollisionEnter(Collision col)
+    {
+        // if the collision object has a player-tag -> notificate the gamecontroller
+        if (col.gameObject.CompareTag("Player"))
+        {
+            gameController.playerDeath();
+        }
+    }
 }
