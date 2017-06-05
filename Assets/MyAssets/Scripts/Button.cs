d2380e7e58f2095d52e168c4cc1ddb8d5c3a6ca1
Salvos <tim.gabriel@t-online.de>
2017-06-05 19:09:53 +0200

    added door-challenge with button

---
+++ Assets/MyAssets/Scripts/Button.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    [Header("Zu triggerndes Object")]
+    public GameObject triggerObject;
+
+    [Range(1, 20)]
+    [Header("Trigger-Delay (in sec.)")]
+    public float triggerDelay = 1;
+
+
+
+
+    /// <summary>
+    /// triggers on collision with the player
+    /// </summary>
+    /// <param name="col"></param>
+    void OnCollisionEnter(Collision col)
+    {
+        // if the collision object has a player-tag -> notificate the gamecontroller
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Door doorTrigger = triggerObject.GetComponent<Door>();
+
+            if(doorTrigger != null)
+            {
+                doorTrigger.trigger();
+            }
+        }
+    }
+
+
+}
