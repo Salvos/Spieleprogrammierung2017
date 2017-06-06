9ce712d4f4f2809a9cddab740fc4ed641e440f57
Salvos <tim.gabriel@t-online.de>
2017-06-06 20:47:17 +0200

    fixed spike animation

--- Assets/MyAssets/Scripts/Player.cs
+++ Assets/MyAssets/Scripts/Player.cs
@@ -22,6 +22,7 @@ public class Player : MonoBehaviour {
     private MyCamera camera;
 
 
+
     ///=================================///
     ///==========Unity Methods==========///
     ///=================================///
