fd1b13d905db0886baceb6bb5341f8707de96b5c
Tim Gabriel <tim.gabriel@t-online.de>
2017-06-06 14:51:35 +0200

    Added door transition - :)

--- Assets/MyAssets/Scripts/Door.cs
+++ Assets/MyAssets/Scripts/Door.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Door : MonoBehaviour {
 
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void trigger()
     {
-
+        animator.SetTrigger("door");
     }
 }
